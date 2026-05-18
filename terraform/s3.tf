6aa4eb749d241e8dd045286d15315c5b51d876c6
pawan <dhuriya.pawan@gmail.com>
2026-05-18 16:26:01 +0530

    testrun

---
+++ terraform/s3.tf
@@ -0,0 +1,10 @@
+resource "aws_s3_bucket" "config_bucket" {
+  bucket = "${var.project_name}-config-bucket"
+}
+
+resource "aws_s3_bucket_versioning" "versioning" {
+  bucket = aws_s3_bucket.config_bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
