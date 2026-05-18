7e6a8bfed23c59d057029391b38059b765e5cabc
pawan <dhuriya.pawan@gmail.com>
2026-05-18 17:08:43 +0530

    script addded .sh

--- scripts/import.sh
+++ scripts/import.sh
@@ -3,12 +3,12 @@ set -euo pipefail
 
 TABLE="cloudguard-violations"
 
-echo "Checking DynamoDB table: $TABLE"
+echo "Checking DynamoDB table..."
 
 if aws dynamodb describe-table --table-name "$TABLE" >/dev/null 2>&1; then
   echo "Table exists → importing into Terraform state"
 
-  terraform import aws_dynamodb_table.violations "$TABLE" || true
+  terraform -chdir=terraform import aws_dynamodb_table.violations "$TABLE" || true
 else
-  echo "Table not found → Terraform will create it"
+  echo "Table does not exist → Terraform will create it"
 fi
