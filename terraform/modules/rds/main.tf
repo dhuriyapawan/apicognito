a9dd53427442471b3e5da60547d01f80635fb8eb
pawan <dhuriya.pawan@gmail.com>
2026-05-20 15:33:10 +0530

    terraform files upload

--- terraform/modules/rds/main.tf
+++ terraform/modules/rds/main.tf
@@ -22,9 +22,9 @@ resource "aws_db_instance" "main" {
   storage_type          = "gp2"
   storage_encrypted     = true
 
-  db_name  = var.db_name
-  username = var.db_username
-  password = var.db_password
+  db_name  = "db"
+  username = "test"
+  password = "Abc123!@"
 
   db_subnet_group_name   = aws_db_subnet_group.main.name
   vpc_security_group_ids = var.security_group_ids
