3d6deb5ff7e34ebfbfed36668ec462faf88c57d4
pawan <dhuriya.pawan@gmail.com>
2026-05-20 19:16:45 +0530

    Terraform

--- terraform/environment/dev/backend.tf
+++ terraform/environment/dev/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "backend-s3-logs "
+    bucket         = "backend-s3-logs"
     key            = "dev/terraform.tfstate"
     region         =  "us-east-1"
     dynamodb_table = "terraform-locks"
