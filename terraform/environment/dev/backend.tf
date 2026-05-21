51bdf2fc4ebe654fe4fd262f3b016456c1e860aa
pawan <dhuriya.pawan@gmail.com>
2026-05-21 11:26:03 +0530

    Terraform

--- terraform/environment/dev/backend.tf
+++ terraform/environment/dev/backend.tf
@@ -4,7 +4,18 @@ terraform {
     key            = "dev/terraform.tfstate"
     region         =  "us-east-1"
     dynamodb_table = "terraform-locks"
-    hash_key ="LockID"
-    # encrypt        = true
+    
+     encrypt        = true
+  }
+}
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "terraform-locks"
+  billing_mode = "PAY_PER_REQUEST"
+
+  hash_key = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
   }
 }
