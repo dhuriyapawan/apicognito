d6784a5ca7f406485f2f8f519be909ae5467021c
pawan <dhuriya.pawan@gmail.com>
2026-05-21 11:15:39 +0530

    Terraform

--- terraform/modules/redis/outputs.tf
+++ terraform/modules/redis/outputs.tf
@@ -13,6 +13,3 @@ output "redis_port" {
 output "security_group_id" {
   value = aws_security_group.redis.id
 }
-output "cluster_security_group_id" {
-  value = aws_eks_cluster.this.vpc_config[0].cluster_security_group_id
-}
